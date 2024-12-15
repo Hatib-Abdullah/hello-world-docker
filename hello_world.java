3cff2d51d729c9a05c91c393495cff668fd2f937
Muhammad Hatib Abdullah <hatibabdullah234@gmail.com>
2024-12-15 17:49:11 +0500

    updated java

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 package com.example;
 
-pubclass HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Pipeline!");
     }
-
+}
