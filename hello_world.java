8484585a79192b2de5bba4fba98a9fc398e18e52
Muhammad Hatib Abdullah <hatibabdullah234@gmail.com>
2024-12-15 17:05:32 +0500

    Error hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class HelloWorld {
+pubclass HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Pipeline!");
     }
